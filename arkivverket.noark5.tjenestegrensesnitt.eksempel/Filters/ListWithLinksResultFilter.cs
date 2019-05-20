c4c1db3d34028f3a4932b94fd51b32345cd11507
Henning Jensen <henning@arkitektum.no>
2019-05-20 13:54:06 +0200

    Let exceptions bubble through ListWithLinksResultFilter

--- arkivverket.noark5.tjenestegrensesnitt.eksempel/Filters/ListWithLinksResultFilter.cs
+++ arkivverket.noark5.tjenestegrensesnitt.eksempel/Filters/ListWithLinksResultFilter.cs
@@ -10,61 +10,26 @@ namespace arkitektum.kommit.noark5.api.Controllers
     /// </summary>
     public class ListWithLinksResultAttribute: ActionFilterAttribute
     {
-        /*
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="actionExecutedContext"></param>
-        public override void OnActionExecuted(ActionExecutedContext actionExecutedContext)
-        {
-            base.OnActionExecuted(actionExecutedContext);
-                dynamic responseContent=null;
-
-                if (actionExecutedContext.Result != null)
-
-
-                    actionExecutedContext.
-                //    responseContent = await actionExecutedContext.Result.ExecuteResultAsync();
-                        //.Content.ReadAsAsync<dynamic>().Result;
-
-
-                if (responseContent != null)
-                {
-
-                    actionExecutedContext
-
-                    var linkList = new List<LinkType>();
-                    var contextLinks = HttpContext.Items["links"];
-                    if (contextLinks != null)
-                    {
-                        linkList.AddRange((List<LinkType>)contextLinks);
-                    }
-
-                    var res = new ListWithLinksResult<dynamic>(responseContent, linkList);
-                 
-                    actionExecutedContext.Response =
-                        actionExecutedContext.Request.CreateResponse(actionExecutedContext.Response.StatusCode,
-                            res);
-                }
-        }*/
-
         public override void OnActionExecuted(ActionExecutedContext context)
         {
             if (context.Result != null)
             {
                 var controllerResult = (ObjectResult)context.Result;
-                var resultList = controllerResult.Value as IEnumerable<dynamic>;
 
-                var links = new List<LinkType>();
-                if (context.HttpContext.Items.ContainsKey("links"))
+                // only run on successful responses - not where the statusCode is explicitly set - typically bad result or something else
+                if (controllerResult.StatusCode == null)
                 {
-                    var contextLinks = (List<LinkType>) context.HttpContext.Items["links"];
-                    links.AddRange(contextLinks);
+                    var resultList = controllerResult.Value as IEnumerable<dynamic>;
+
+                    var links = new List<LinkType>();
+                    if (context.HttpContext.Items.ContainsKey("links"))
+                    {
+                        var contextLinks = (List<LinkType>) context.HttpContext.Items["links"];
+                        links.AddRange(contextLinks);
+                    }
+
+                    context.Result = new OkObjectResult(new ListWithLinksResult<dynamic>(resultList, links));
                 }
-
-                context.Result = new OkObjectResult(new ListWithLinksResult<dynamic>(resultList, links));
-
 
             }
         }
